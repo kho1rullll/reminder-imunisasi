4b3b9c66f262f2d90003212a1fdecbede430a704
kho1rullll <khoirulbarie2102@gmail.com>
2026-04-26 20:23:31 +0700

    update sedikit

--- api/dashboard.php
+++ api/dashboard.php
@@ -50,6 +50,7 @@ $query_riwayat = mysqli_query($koneksi, "SELECT * FROM jadwal_imunisasi
     <title>Dashboard – ImunisasiKu</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image" href="images/Favicon.png">
     <script>
         tailwind.config = {
         theme: {
--- api/register.php
+++ api/register.php
@@ -7,6 +7,7 @@
     <title>Register – ImunisasiKu</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image" href="images/Favicon.png">
     <script>
         tailwind.config = {
         theme: {
